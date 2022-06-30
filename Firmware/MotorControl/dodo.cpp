60e84ae1ed393d5464199233fc268e74f6a4f760
Timm Plagge <timm.plagge@tum.de>
2022-06-30 18:21:22 +0200

    Cleaned up dodo.hpp, started adding code for foc data sent to PC

--- Firmware/MotorControl/dodo.cpp
+++ Firmware/MotorControl/dodo.cpp
@@ -1,12 +1,10 @@
 #include "dodo.hpp"
 
 Dodo::Dodo() {
-    test_val_ = 1000;
-
 }
 
 void Dodo::test_incr() {
     test_val_ += 1;
-    foc_.i_alpha += 1.0;
-    foc_.i_beta += 1.0;
+    // foc_.i_alpha += 1.0f;
+    // foc_.i_beta += 1.0f;
 }
